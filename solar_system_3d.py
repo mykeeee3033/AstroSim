e869d2537d98168e1e460b9133364969e7cf2b87
Micheal Tidwell <109658326+mykeeee3033@users.noreply.github.com>
2024-07-03 23:47:03 +0700

    Update solar_system_3d.py
    broken file, many errors with simulation ending as sun/planet moves close to z-axis. Moreover, after a few attempts, the simulation broke completely. 
    
    NEEDS OVERHAUL AND REVISION + STUDY

--- solar_system_3d.py
+++ solar_system_3d.py
@@ -1,58 +1,73 @@
-# solar_system_3d.py
-
 import math
 import matplotlib.pyplot as plt
-from solar_system_3d import SolarSystem
 from vectors import Vector
 
 class SolarSystem:
     def __init__(self, size):
         self.size = size
         self.bodies = []
-        self.fig, self.ax = plt.subplots(
-            1,
-            1,
-            subplot_kw={"projection": "3d"},
-            figsize=(self.size / 50, self.size / 50),
-        )
-        self.fig.tight_layout()
+        self.fig = plt.figure(figsize=(8, 8))
+        self.ax = self.fig.add_subplot(111, projection='3d')
+    
     def add_body(self, body):
         self.bodies.append(body)
+    
+    def calculate_all_body_interactions(self):
+        bodies_copy = self.bodies.copy()
+        for idx, first in enumerate(bodies_copy):
+            for second in bodies_copy[idx + 1:]:
+                first.accelerate_due_to_gravity(second)
+    
+    def update_all(self):
+        self.bodies.sort(key=lambda item: item.position[0])
+        for body in self.bodies:
+            body.move()
+            body.draw()
+    
+    def draw_all(self):
+        self.ax.set_xlim((-self.size / 2, self.size / 2))
+        self.ax.set_ylim((-self.size / 2, self.size / 2))
+        self.ax.set_zlim((-self.size / 2, self.size / 2))
+        self.ax.axis(False)
+        plt.pause(0.001)
+        self.ax.clear()
 
 class SolarSystemBody:
-    min_display_size = 10
-    display_log_base = 1.3
-
-    def __init__(
-        self,
-        solar_system,
-        mass,
-        position=(0, 0, 0),
-        velocity=(0, 0, 0),
-    ):
+    def __init__(self, solar_system, mass, position=(0, 0, 0), velocity=(0, 0, 0)):
         self.solar_system = solar_system
         self.mass = mass
         self.position = position
         self.velocity = Vector(*velocity)
-        self.display_size = max(
-            math.log(self.mass, self.display_log_base),
-            self.min_display_size,
-        )
-        self.colour = "black"
-
-        self.solar_system.add_body(self)
-
+    
     def move(self):
-        self.position = (
-            self.position[0] + self.velocity[0],
-            self.position[1] + self.velocity[1],
-            self.position[2] + self.velocity[2],
-        )
-
+        self.position = tuple(map(sum, zip(self.position, self.velocity)))
+    
     def draw(self):
         self.solar_system.ax.plot(
             *self.position,
             marker="o",
-            markersize=self.display_size,
+            markersize=self.display_size + self.position[0] / 30,
             color=self.colour
         )
+    
+    def accelerate_due_to_gravity(self, other):
+        distance = Vector(*other.position) - Vector(*self.position)
+        distance_mag = distance.get_magnitude()
+        force_mag = self.mass * other.mass / (distance_mag ** 2)
+        force = distance.normalize() * force_mag
+        for body in (self, other):
+            acceleration = force / body.mass
+            body.velocity += acceleration * (-1 if body is self else 1)
+
+class Sun(SolarSystemBody):
+    def __init__(self, solar_system, mass=10_000, position=(0, 0, 0), velocity=(0, 0, 0)):
+        super().__init__(solar_system, mass, position, velocity)
+        self.colour = "yellow"
+
+class Planet(SolarSystemBody):
+    colours = iter([(1, 0, 0), (0, 1, 0), (0, 0, 1)])
+
+    def __init__(self, solar_system, mass=10, position=(0, 0, 0), velocity=(0, 0, 0)):
+        super().__init__(solar_system, mass, position, velocity)
+        self.colour = next(Planet.colours)
+
